e0c02a101c96052735077dd92c6b2cb7e4c3c9b3
loscarnoel <35186809+loscarnoel@users.noreply.github.com>
2018-01-07 03:58:32 -0300

    Add files via upload

---
+++ aa.js
@@ -0,0 +1,4 @@
+alert("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+var hola = 87;
+document.write(hola * 1432);
+//5 492 616 950 742
